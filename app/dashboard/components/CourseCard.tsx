b255d5cfb215c1bfc8af8490e6a2c8ca1f29521b
Dearie <eburujacintha@gmail.com>
2025-12-01 15:11:21 +0400

    made fixes

--- app/dashboard/components/CourseCard.tsx
+++ app/dashboard/components/CourseCard.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import { PlayIcon, ClockIcon } from '@heroicons/react/24/solid';
 
--- app/dashboard/components/StatCard.tsx
+++ app/dashboard/components/StatCard.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import { ArrowUpIcon, ArrowDownIcon } from '@heroicons/react/24/outline';
 import { BoltIcon, ClockIcon, TrophyIcon, QuestionMarkCircleIcon } from '@heroicons/react/24/solid';
